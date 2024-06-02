15589d0a2fabdc877ec2378e5df6c348ba33210b
KimYouGyoung <tptkds12@gmail.com>
2024-06-03 00:37:01 +0900

    💄 style(ui): Add header UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './globals.css';
 import { Noto_Sans_KR } from 'next/font/google';
+import { Header } from '@/widgets';
+
 const NOTO_SANS_KR = Noto_Sans_KR({
   preload: true,
   subsets: ['latin'],
@@ -13,7 +15,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={NOTO_SANS_KR.className}>{children}</body>
+      <body className={NOTO_SANS_KR.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ src/widgets/header/ui/Logo.tsx
@@ -0,0 +1,3 @@
+export default function Logo() {
+  return <div className="h-[80px] w-[120px] bg-gray-100">로고</div>;
+}
